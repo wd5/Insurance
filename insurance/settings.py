08e40247f66ee4cc63cc54a7b9dafa6f20438fa7
Vladimir Kazanov <vkazanov@inbox.ru>
2011-06-29 01:04:29 +0400

    Базовая структура файлов и макеты конфигурационных файлов

---
+++ insurance/settings.py
@@ -0,0 +1,69 @@
+import os
+import sys
+
+# A little hack to put apps into the app dir
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.join(PROJECT_PATH, "apps"))
+
+# Usual Django settings
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
+
+TIME_ZONE = None
+LANGUAGE_CODE = 'en-us'
+SITE_ID = 1
+USE_I18N = False
+USE_L10N = False
+
+# Static/media files urls/paths
+MEDIA_ROOT = os.path.join(PROJECT_PATH, '../media')
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(PROJECT_PATH, '../static')
+STATIC_URL = '/static/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static'),
+)
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'insurance.urls'
+
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_PATH, 'templates'),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+)
+
+# Import deploy-specific settings, if present
+try:
+    from local_settings import *
+except ImportError, e:
+    pass
