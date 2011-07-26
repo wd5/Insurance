2dfe4f49d9567ef4da17143b0d91dbe03e0165ae
Evgeny Kazanov <evgeny.kazanov@gmail.com>
2011-07-26 15:34:08 +0300

    Добавлен почтовый ящик inbox

---
+++ insurance/apps/ins_notification/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.defaults import *
+
+from notification.views import notices, mark_all_seen, feed_for_user, single, notice_settings
+from ins_notification.views import inbox,question
+
+
+urlpatterns = patterns('',
+    url(r'^$', inbox, name="ins_notification_inbox"),
+    url(r'^$', question, name="ins_notification_question"),
+    # url(r'^$', notices, name="notification_notices"),
+    # url(r'^settings/$', notice_settings, name="notification_notice_settings"),
+    # url(r'^(\d+)/$', single, name="notification_notice"),
+    # url(r'^feed/$', feed_for_user, name="notification_feed_for_user"),
+    # url(r'^mark_all_seen/$', mark_all_seen, name="notification_mark_all_seen"),
+)
--- insurance/apps/ins_notification/views.py
+++ insurance/apps/ins_notification/views.py
@@ -1,1 +1,22 @@
-# Create your views here.
+from django.views.generic.simple import direct_to_template
+from django.contrib.auth.decorators import login_required
+from notification.models import Notice
+
+@login_required
+def inbox(request):
+    notices = Notice.objects.notices_for(request.user, sent=True)
+    
+    extra_content = {'notices':notices}
+    return direct_to_template(request,
+                              'notification/inbox.html',
+                              extra_content)
+
+
+@login_required
+def question(request):
+    """
+    
+    Arguments:
+    - `request`:
+    """
+    pass
