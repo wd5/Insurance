9abfd559f2a93359350ba25bedd047bc9efb81cf
Sergey Maltsev <alternativshik@gmail.com>
2011-10-13 13:51:02 +0400

    полисы в админке

--- insurance/apps/polices/admin.py
+++ insurance/apps/polices/admin.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from models import InsurancePolicy
-#
-#class InsurancePolicyAdmin(admin.ModelAdmin):
-#    list_display = ('user', 'type', 'state',)
-#    readonly_fields = ("user", "type", "payment", "state", "company", "mark",
-#                       "model", "model_year", "power", "price", "wheel", "city",
-#                       "credit", "unlimited_users", "age", "experience_driving",
-#                       "age1", "experience_driving1", "age2",
-#                       "experience_driving2", "age3", "experience_driving3",
-#                       "vin", "body_type", "mileage", "last_name", "first_name",
-#                       "middle_name", "birth_date", "sex", "first_owner",
-#                       "reg_address", "liv_address", "pol_address")
-#
-#admin.site.register(InsurancePolicy, InsurancePolicyAdmin)
+from models import InsurancePolicy, InsurancePolicyData
+
+class InsurancePolicyAdmin(admin.ModelAdmin):
+    list_display = ('user', 'type', 'state',)
+    readonly_fields = ("user", "type", "payment", "state", "company", "mark",
+                       "model", "model_year", "power", "price", "wheel", "city",
+                       "credit", "unlimited_users", "age", "experience_driving",
+                       "age1", "experience_driving1", "age2",
+                       "experience_driving2", "age3", "experience_driving3"
+                      )
+admin.site.register(InsurancePolicy, InsurancePolicyAdmin)
